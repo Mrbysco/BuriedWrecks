97c84df1c5d66bb5fefa15aafa59c582462b1773
Mrbysco <Mrbysco@users.noreply.github.com>
2023-06-27 20:12:36 +0200

    Update to 1.19.4 [build] [publish]

--- src/main/java/com/mrbysco/buriedwrecks/registry/ModStructureSets.java
+++ src/main/java/com/mrbysco/buriedwrecks/registry/ModStructureSets.java
@@ -6,7 +6,6 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureSet;
 import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadStructurePlacement;
@@ -19,8 +18,6 @@ public class ModStructureSets {
 
 	public static void bootstrap(BootstapContext<StructureSet> context) {
 		HolderGetter<Structure> structureGetter = context.lookup(Registries.STRUCTURE);
-		HolderGetter<Biome> biomeGetter = context.lookup(Registries.BIOME);
-
 		context.register(BURIED_SHIPWRECKS, new StructureSet(List.of(StructureSet.entry(structureGetter.getOrThrow(ModStructures.BURIED_SHIPWRECK))),
 				new RandomSpreadStructurePlacement(24, 4, RandomSpreadType.LINEAR, 15869703)));
 
--- src/main/java/com/mrbysco/buriedwrecks/util/BuriedBiomeTags.java
+++ src/main/java/com/mrbysco/buriedwrecks/util/BuriedBiomeTags.java
@@ -7,9 +7,9 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
 
 public class BuriedBiomeTags {
-	public static final TagKey<Biome> HAS_BURIED_SHIPWRECK = create(BuriedWrecks.MOD_ID + ":has_structure/buried_shipwreck");
+	public static final TagKey<Biome> HAS_BURIED_SHIPWRECK = create("has_structure/buried_shipwreck");
 
 	private static TagKey<Biome> create(String id) {
-		return TagKey.create(Registries.BIOME, new ResourceLocation(id));
+		return TagKey.create(Registries.BIOME, new ResourceLocation(BuriedWrecks.MOD_ID, id));
 	}
 }
